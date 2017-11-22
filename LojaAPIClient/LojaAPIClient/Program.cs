754707791b814160e68442a7f54dc533f8aa88ac
Giuliano Gusmão <gusmaoggg@gmail.com>
2017-11-22 19:54:15 -0200

    ApiClient - projeto final

--- LojaAPIClient/LojaAPIClient/Program.cs
+++ LojaAPIClient/LojaAPIClient/Program.cs
@@ -12,8 +12,27 @@ namespace LojaAPIClient
     {
         static void Main(string[] args)
         {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:60852/api/carrinho/1/produto/6237/quantidade");
+            request.Method = "PUT";
+
+            string xml = "<Produto xmlns:i='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://schemas.datacontract.org/2004/07/Loja.Models'><Id>6237</Id><Nome>Videogame 4</Nome><Preco>4000</Preco><Quantidade>10</Quantidade></Produto>";
+
+            byte[] xmlBytes = Encoding.UTF8.GetBytes(xml);
+            request.GetRequestStream().Write(xmlBytes, 0, xmlBytes.Length);
+
+            request.ContentType = "Application/xml";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+
+            Console.Read();
+        }
+
+        static void TestaGet(string[] args)
+        {
             string conteudo;
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.mocky.io/v2/52aaf5deee7ba8c70329fb7d");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:60852/api/carrinho/1");
             request.Method = "GET";
 
             WebResponse response = request.GetResponse();

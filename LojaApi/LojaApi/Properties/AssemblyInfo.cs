b74c15d7078280eb81d50899a0401611333d8594
Giuliano Gusmão <gusmaoggg@gmail.com>
2017-11-22 18:04:07 -0200

    corrigindo bug e alterando versao do projeto para 4.5.2

--- LojaApi/LojaApi/Properties/AssemblyInfo.cs
+++ LojaApi/LojaApi/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d75d9fba-aaf0-489d-a36e-d08f5674a6fe")]
+[assembly: Guid("e230fd3a-6374-478b-bf80-81d3468b2979")]
 
 // Version information for an assembly consists of the following four values:
 //
